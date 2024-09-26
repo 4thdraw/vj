4cb0b246270779fd68b81fab9299f846d4997b1a
4thdraw <4thdraw2@gmail.com>
2024-09-26 11:43:46 +0900

    패밀리사이트 스타일 구현

--- src/basic.js
+++ src/basic.js
@@ -13,11 +13,11 @@ window.addEventListener('load', function(){
   }
   this.document.querySelector("#family ul").innerHTML = familylist;
 
-  
 
-  
-
-
+  //패밀리노출
+  this.document.querySelector("#family").addEventListener('click', function(){
+    this.classList.toggle('open');
+  })
 
 })
 
